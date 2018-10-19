24b3800e9a1eae9244469769c673342d5aae40b8
mackjkl <mackjkl@mackjkls-imac.local>
2018-10-19 23:18:01 +0200

    Organize project structure and add assets

---
+++ whats-the-weather/Models/Location.swift
@@ -0,0 +1,9 @@
+//
+//  Location.swift
+//  whats-the-weather
+//
+//  Created by mackjkl on 10/19/18.
+//  Copyright © 2018 kjkl. All rights reserved.
+//
+
+import Foundation
---
+++ whats-the-weather/Services/DataFetcher.swift
@@ -0,0 +1,9 @@
+//
+//  DataFetcher.swift
+//  whats-the-weather
+//
+//  Created by mackjkl on 10/19/18.
+//  Copyright © 2018 kjkl. All rights reserved.
+//
+
+import Foundation
