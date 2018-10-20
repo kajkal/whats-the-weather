a479df9e2ecaa63ab513f5a9ef513ba5d7a1fd7e
mackjkl <mackjkl@mackjkls-imac.local>
2018-10-20 19:45:28 +0200

    detail view connect with code 1

--- whats-the-weather/Views/Detail/DetailViewController.swift
+++ whats-the-weather/Views/Detail/DetailViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var locationNameLbl: UINavigationItem!
-
+    @IBOutlet weak var locationNameNavItem: UINavigationItem!
+    @IBOutlet weak var dateFullLbl: UILabel!
+    @IBOutlet weak var dateDayNameLbl: UILabel!
+    @IBOutlet weak var weatherStateIconImg: UIImageView!
+    @IBOutlet weak var tempMinLbl: UILabel!
+    @IBOutlet weak var tempMaxLbl: UILabel!
+    @IBOutlet weak var windSpeedLbl: UILabel!
+    @IBOutlet weak var windDirectionLbl: UILabel!
+    @IBOutlet weak var humidityLbl: UILabel!
+    @IBOutlet weak var airPressureLbl: UILabel!
+    @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    private let weather: Weather
+    
 
     func configureView() {
+        print("configureView")
         // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
+//        if let detail = detailItem {
+//            if let label = detailDescriptionLabel {
+//                label.text = detail.description
+//            }
+//        }
     }
 
     override func viewDidLoad() {
